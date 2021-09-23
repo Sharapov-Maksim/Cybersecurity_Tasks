f18d604943b23ccb130f69f80937b8b47f61b308
Sharapov-Maksim <m.sharapov.nvk@yandex.ru>
2021-09-23 20:04:01 +0700

    Приведено в более близкий к оффициальной документации вид
    Что-то было исправлено, но по прежнему работает неправильно =(

--- task1_SHA-3/sha-3.py
+++ task1_SHA-3/sha-3.py
@@ -71,25 +71,60 @@ RC.append(0x8000000000008080)
 RC.append(0x0000000080000001)
 RC.append(0x8000000080008008)
 
+def rot(a, shift, N = 2**l):
+                          # a=  |111001|    shift = 1
+    b = a << shift        #    1|110010|
+    m = 1 << N            #    1|000000|
+    c = b % m             #    0|110010|
+    d = a >> (N - shift)  #     |000001|(11001)
+    res = c | d           #     |110011|
+    return res
+
+r = [[0,36,3,41,18],
+     [1,44,10,45,2],
+     [62,6,43,15,61],
+     [28,55,25,21,56],
+     [27,20,39,8,14]]
 
 def f(A, iterations_number = 24):
     for iteration in range(0, iterations_number):
         # step theta
+        '''
         for i in range(5):
             for j in range(5):
                 for k in range(2**l):
                     for n in range(5):
                         A[i][j] ^= (A[n][(j-1)%5] & (1<<k)) ^ (A[n][(j+1)%5] & (1<<((k-1)%5)))
-        # step rho  ( << (t+1)(t+2)//2 )
+        '''
+        C = []
+        for x in range(5):
+            C.append(A[x][0] ^ A[x][1] ^ A[x][2] ^ A[x][3] ^ A[x][4])
+        D = [0, 0, 0, 0, 0]
+        for x in range(5):
+            D[x] = C[(x-1)%5] ^ rot(C[(x+1)%5], 1)
+        for x in range(5):
+            for y in range(5):
+                A[x][y] ^= D[x]
 
+        # step rho + pi  ( << (t+1)(t+2)//2 )
+        B = []
+        for i in range(5):
+            B.append([0,0,0,0,0])
+
+        for x in range(5):
+            for y in range(5):
+                B[y][(2*x+3*y)%5] = rot(A[x][y], r[x][y])
+
+        '''
         tmp = [[A[0][0],0,0,0,0]]
         for r in range(4):
             tmp.append([0,0,0,0,0])
         i = 1
         j = 0
         for t in range(24):
-            for k in range(2**l):
-                tmp[i][j] |= (((A[i][j] & (1<<((k - (t+1)*(t+2)//2) % 2**l))) >> ((k - (t+1)*(t+2)//2) % 2**l)) << k)
+            tmp[i][j] = rot(A[i][j], ((t+1)*(t+2)//2) % 2**l)
+            #for k in range(2**l):
+            #    tmp[i][j] |= (((A[i][j] & (1<<((k - (t+1)*(t+2)//2) % 2**l))) >> ((k - (t+1)*(t+2)//2) % 2**l)) << k)
             iprev = i
             i = (3*i + 2*j) % 5
             j = iprev
@@ -97,14 +132,13 @@ def f(A, iterations_number = 24):
         for x in range(5):
             for y in range(5):
                 A[y][(2*x+3*y)%5] = tmp[x][y]
+        '''
         # step chi
         for x in range(5):
             for y in range(5):
-                tmp[x][y] = A[x][y] ^ (~A[(x+1)%5][y] & A[(x+2)%5][y])
-        A = tmp
+                A[x][y] = B[x][y] ^ ((~B[(x+1)%5][y]) & B[(x+2)%5][y])
         # step iota
         A[0][0] ^= RC[iteration]
-
     return A
 
 state = []
@@ -122,16 +156,14 @@ for blockId in range (0, blocksNum):
                 state[i][j] ^= block[i][j]
     else:
         state = block
-    print("Before f: ", state)
+    #print("Before f: ", state)
     state = f(state)
-    print("After  f: ", state)
+    #print("After  f: ", state)
 
 res = 0
 for i in range(4):
     res |= state[0][i] << (3-i) * 64
 
 print(res.to_bytes(32, "big").hex())
-
-
 
 
