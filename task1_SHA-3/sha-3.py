d1161cb01e1e5bf79b5402bfc242f2ca3feae0b0
Sharapov-Maksim <55018975+Sharapov-Maksim@users.noreply.github.com>
2021-09-30 21:36:17 +0700

    Update sha-3.py

--- task1_SHA-3/sha-3.py
+++ task1_SHA-3/sha-3.py
@@ -1,26 +1,16 @@
-
-
 # output length: 256 bits
 b = 1600
 l = 6
 nrounds = 24
 blockSize = 1088
 capacity = b - blockSize
-file = open('input.txt', 'rb')
+file = open('big_input.txt', 'rb')
 inputMessage = bytearray(file.read())
 y = int.from_bytes(inputMessage[0:8], "big", signed=True)
 
 
-'''
-inputBits = []
-for i in range(0, len(inputMessage)):
-    byte = format(inputMessage[i], '08b')
-    inputBits.extend([int(b) for b in byte])
-'''
-print(len(inputMessage)*8)
 # Adding padding
 paddingNumber = blockSize - (len(inputMessage)*8 % blockSize)  # count of bits to add
-print(paddingNumber)
 if paddingNumber == 0:
     inputMessage.join(bytes([l]))
     for i in range(1, blockSize//8 - 1):
@@ -36,10 +26,7 @@ else:
         inputMessage.append(0b00000000)
     inputMessage.append(0x80)
 
-print(len(inputMessage)*8)
 blocksNum = (len(inputMessage)*8) // blockSize
-print(blocksNum)
-print(inputMessage)
 
 RC = []
 RC.append(0x0000000000000001)
@@ -85,13 +72,6 @@ r = [[0,36,3,41,18],
 def f(A, iterations_number = 24):
     for iteration in range(0, iterations_number):
         # step theta
-        '''
-        for i in range(5):
-            for j in range(5):
-                for k in range(2**l):
-                    for n in range(5):
-                        A[i][j] ^= (A[n][(j-1)%5] & (1<<k)) ^ (A[n][(j+1)%5] & (1<<((k-1)%5)))
-        '''
         C = []
         for x in range(5):
             C.append(A[x][0] ^ A[x][1] ^ A[x][2] ^ A[x][3] ^ A[x][4])
@@ -111,24 +91,6 @@ def f(A, iterations_number = 24):
             for y in range(5):
                 B[y][(2*x+3*y)%5] = rot(A[x][y], r[x][y])
 
-        '''
-        tmp = [[A[0][0],0,0,0,0]]
-        for r in range(4):
-            tmp.append([0,0,0,0,0])
-        i = 1
-        j = 0
-        for t in range(24):
-            tmp[i][j] = rot(A[i][j], ((t+1)*(t+2)//2) % 2**l)
-            #for k in range(2**l):
-            #    tmp[i][j] |= (((A[i][j] & (1<<((k - (t+1)*(t+2)//2) % 2**l))) >> ((k - (t+1)*(t+2)//2) % 2**l)) << k)
-            iprev = i
-            i = (3*i + 2*j) % 5
-            j = iprev
-        # step pi
-        for x in range(5):
-            for y in range(5):
-                A[y][(2*x+3*y)%5] = tmp[x][y]
-        '''
         # step chi
         for x in range(5):
             for y in range(5):
@@ -145,7 +107,6 @@ for blockId in range (0, blocksNum):
         block.append(int.from_bytes(blockBytes[start : start+8], "little", signed=False))
     for start in range(blockSize//8, b//8, 8):
         block.append(0)
-    #lanes = [[load64(state[8 * (x + 5 * y):8 * (x + 5 * y) + 8]) for y in range(5)] for x in range(5)]
     block = [block[i:21+i:5] for i in range(0, 5)]
     if blockId != 0:
         for i in range(5):
@@ -153,13 +114,11 @@ for blockId in range (0, blocksNum):
                 state[i][j] ^= block[i][j]
     else:
         state = block
-    #print("Before f: ", state)
     state = f(state)
-    #print("After  f: ", state)
 
 res = 0
 for i in range(4):
-    res |= state[i][0] << i * 64
+    res |= state[i][0] << (i * 64)
 
 print(res.to_bytes(32, "little").hex())
 
