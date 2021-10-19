633b9469579749db0d13b1a6992c71c5e924e3a3
Sharapov-Maksim <55018975+Sharapov-Maksim@users.noreply.github.com>
2021-10-19 19:00:39 +0700

    Update sha-3.py
    В основном косметические изменения

--- task1_SHA-3/sha-3.py
+++ task1_SHA-3/sha-3.py
@@ -6,20 +6,20 @@ blockSize = 1088
 capacity = b - blockSize
 file = open('big_input.txt', 'rb')
 inputMessage = bytearray(file.read())
-y = int.from_bytes(inputMessage[0:8], "big", signed=True)
-
+print("Size of input: " + str(len(inputMessage)) + " Bytes")
 
 # Adding padding
 paddingNumber = blockSize - (len(inputMessage)*8 % blockSize)  # count of bits to add
 if paddingNumber == 0:
-    inputMessage.join(bytes([l]))
+    inputMessage.append(l)
     for i in range(1, blockSize//8 - 1):
-        inputMessage.join(bytes([0b00000000]))
-    inputMessage.join(bytes([0x80]))
+        inputMessage.append(0b00000000)
+    inputMessage.append(0x80)
 elif paddingNumber == 8:
-    inputMessage.join(bytes([l ^ 0x80]))
+    inputMessage.append(l ^ 0x80)
 elif paddingNumber == 16:
-    inputMessage.join(bytes([l, 0x80]))
+    inputMessage.append(l)
+    inputMessage.append(0x80)
 else:
     inputMessage.append(l)
     for i in range(1, paddingNumber // 8 - 1):
@@ -27,34 +27,41 @@ else:
     inputMessage.append(0x80)
 
 blocksNum = (len(inputMessage)*8) // blockSize
+# Round constants
+RC = [0 for i in range(24)]
+RC[0]  = 0x0000000000000001
+RC[1]  = 0x0000000000008082
+RC[2]  = 0x800000000000808A
+RC[3]  = 0x8000000080008000
+RC[4]  = 0x000000000000808B
+RC[5]  = 0x0000000080000001
+RC[6]  = 0x8000000080008081
+RC[7]  = 0x8000000000008009
+RC[8]  = 0x000000000000008A
+RC[9]  = 0x0000000000000088
+RC[10] = 0x0000000080008009
+RC[11] = 0x000000008000000A
+RC[12] = 0x000000008000808B
+RC[13] = 0x800000000000008B
+RC[14] = 0x8000000000008089
+RC[15] = 0x8000000000008003
+RC[16] = 0x8000000000008002
+RC[17] = 0x8000000000000080
+RC[18] = 0x000000000000800A
+RC[19] = 0x800000008000000A
+RC[20] = 0x8000000080008081
+RC[21] = 0x8000000000008080
+RC[22] = 0x0000000080000001
+RC[23] = 0x8000000080008008
 
-RC = []
-RC.append(0x0000000000000001)
-RC.append(0x0000000000008082)
-RC.append(0x800000000000808A)
-RC.append(0x8000000080008000)
-RC.append(0x000000000000808B)
-RC.append(0x0000000080000001)
-RC.append(0x8000000080008081)
-RC.append(0x8000000000008009)
-RC.append(0x000000000000008A)
-RC.append(0x0000000000000088)
-RC.append(0x0000000080008009)
-RC.append(0x000000008000000A)
-RC.append(0x000000008000808B)
-RC.append(0x800000000000008B)
-RC.append(0x8000000000008089)
-RC.append(0x8000000000008003)
-RC.append(0x8000000000008002)
-RC.append(0x8000000000000080)
-RC.append(0x000000000000800A)
-RC.append(0x800000008000000A)
-RC.append(0x8000000080008081)
-RC.append(0x8000000000008080)
-RC.append(0x0000000080000001)
-RC.append(0x8000000080008008)
+# Rotation offsets
+r = [[0,36,3,41,18],
+     [1,44,10,45,2],
+     [62,6,43,15,61],
+     [28,55,25,21,56],
+     [27,20,39,8,14]]
 
-def rot(a, shift, N = 2**l):
+def rot(a, shift, N = 64):
                           # a=  |111001|    shift = 1
     b = a << shift        #    1|110010|
     m = 1 << N            #    1|000000|
@@ -63,11 +70,6 @@ def rot(a, shift, N = 2**l):
     res = c | d           #     |110011|
     return res
 
-r = [[0,36,3,41,18],
-     [1,44,10,45,2],
-     [62,6,43,15,61],
-     [28,55,25,21,56],
-     [27,20,39,8,14]]
 
 def f(A, iterations_number = 24):
     for iteration in range(0, iterations_number):
@@ -99,6 +101,7 @@ def f(A, iterations_number = 24):
         A[0][0] ^= RC[iteration]
     return A
 
+
 state = []
 for blockId in range (0, blocksNum):
     blockBytes = inputMessage[blockId * (blockSize // 8) : (blockId+1) * (blockSize // 8)]
@@ -119,7 +122,7 @@ for blockId in range (0, blocksNum):
 res = 0
 for i in range(4):
     res |= state[i][0] << (i * 64)
-
+print("Result:")
 print(res.to_bytes(32, "little").hex())
 
 
